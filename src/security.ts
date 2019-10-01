973ed34a45eabdff400ace938f36ed210259923b
Phillip Henslee <ph2@ph2.us>
2019-10-01 16:42:01 -0500

    move to airbnb

--- src/security.ts
+++ src/security.ts
@@ -4,26 +4,31 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { InvestmentSecurity } from './types'
+import { InvestmentSecurity } from './types';
 
 /**
  * @class Security
  */
 export class Security implements InvestmentSecurity {
-  latestPrice: number
-  previousClose: number
-  changeDay: number
-  changeDayPercent: number
-  priceHigh: number
-  priceLow: number
+  latestPrice: number;
+
+  previousClose: number;
+
+  changeDay: number;
+
+  changeDayPercent: number;
+
+  priceHigh: number;
+
+  priceLow: number;
 
   constructor(public name: string, public symbol: string) {
-    this.changeDay = 0
-    this.changeDayPercent = 0
-    this.latestPrice = 0
-    this.priceHigh = 0
-    this.priceLow = 0
-    this.previousClose = 0
+    this.changeDay = 0;
+    this.changeDayPercent = 0;
+    this.latestPrice = 0;
+    this.priceHigh = 0;
+    this.priceLow = 0;
+    this.previousClose = 0;
   }
 
   update(values: []) {

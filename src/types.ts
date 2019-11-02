959e422c75e474351676fea40405ca94773ee655
Phillip Henslee <ph2@ph2.us>
2019-11-02 18:21:28 -0500

    add quote

--- src/types.ts
+++ src/types.ts
@@ -35,3 +35,47 @@ export interface InvestmentSecurity {
   priceHigh: number;
   priceLow: number;
 }
+export interface quote {
+  symbol: string;
+  companyName: string;
+  calculationPrice: string;
+  open: number;
+  openTime: number;
+  close: number;
+  closeTime: number;
+  high: number;
+  low: number;
+  latestPrice: number;
+  latestSource: string;
+  latestTime: string;
+  latestUpdate: number;
+  latestVolume: number;
+  volume: number;
+  iexRealtimePrice: number;
+  iexRealtimeSize: number;
+  iexLastUpdated: number;
+  delayedPrice: number;
+  delayedPriceTime: number;
+  extendedPrice: number;
+  extendedChange: number;
+  extendedChangePercent: number;
+  extendedPriceTime: number;
+  previousClose: number;
+  previousVolume: number;
+  change: number;
+  changePercent: number;
+  iexMarketPercent: number;
+  iexVolume: number;
+  avgTotalVolume: number;
+  iexBidPrice: number;
+  iexBidSize: number;
+  iexAskPrice: number;
+  iexAskSize: number;
+  marketCap: number;
+  week52High: number;
+  week52Low: number;
+  ytdChange: number;
+  peRatio: number;
+  lastTradeTime: number;
+  isUSMarketOpen: boolean;
+}
